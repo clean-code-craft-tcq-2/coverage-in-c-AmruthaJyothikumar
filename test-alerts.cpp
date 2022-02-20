1d08d17aebc94b945979ba7e7b8fc9bcf1d4e2ef
AmruthaJyothikumar <90920630+AmruthaJyothikumar@users.noreply.github.com>
2022-02-20 14:24:45 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -24,6 +24,7 @@ TEST_CASE("classify the temperature breach") {
 }
 
 TEST_CASE("alters breachtype") {
+  BatteryCharacter batteryChar;
   batteryChar.coolingType =  PASSIVE_COOLING;
   batteryChar.brand = "EXIDE";
   REQUIRE(checkAndAlert(TO_CONTROLLER, batteryChar, 40) == 1);
